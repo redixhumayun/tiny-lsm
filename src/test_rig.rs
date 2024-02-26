81b8c97282780f493e583f8b86938fb4775ff2dc
Zaid Humayun <redixhumayun@gmail.com>
2024-02-26 20:42:09 +0530

    refactored some of the code

--- src/test_rig.rs
+++ src/test_rig.rs
@@ -1,14 +1,9 @@
-use crate::block::{Block, ENTRY_SIZE_AVAILABLE};
-
-pub fn generate_key_value_pairs() {
-    let mut block = Block::new();
-    let total_possible_entries = ENTRY_SIZE_AVAILABLE / 21;
-    println!("Total possible entries {}", total_possible_entries);
-    for i in 0..total_possible_entries {
+pub fn generate_key_value_pairs(num_of_entries: usize) -> Vec<(String, String)> {
+    let mut vec: Vec<(String, String)> = Vec::new();
+    for i in 0..num_of_entries {
         let key: &str = &format!("key_{:<05}", i);
         let value: &str = &format!("value_{:<05}", i);
-        assert_eq!(block.add(key, value), true);
+        vec.push((key.to_string(), value.to_string()));
     }
-    //  should not be able to add any more key value pairs
-    assert_eq!(block.add("key_00000", "value_00000"), false);
+    vec
 }
